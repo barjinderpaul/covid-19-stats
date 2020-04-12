37c409830cac5a833b07293cb4c9d99683bfb57b
barjinderpaul <barjinder.singh@magicpin.in>
2020-04-12 19:27:17 +0530

    Updates

---
+++ precache-manifest.04dbc9d3d3574cc11cd4621e34aaa59b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e9ff27b5b7c852bb840de1d00dbfdc2e",
+    "url": "/covid-19-stats/index.html"
+  },
+  {
+    "revision": "f0bedefdac6d85c231c3",
+    "url": "/covid-19-stats/static/css/main.94031ebb.chunk.css"
+  },
+  {
+    "revision": "be714aea87d29622578a",
+    "url": "/covid-19-stats/static/js/2.707520c3.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/covid-19-stats/static/js/2.707520c3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f0bedefdac6d85c231c3",
+    "url": "/covid-19-stats/static/js/main.d5e3735a.chunk.js"
+  },
+  {
+    "revision": "dd55ea1cb05b941b2b01",
+    "url": "/covid-19-stats/static/js/runtime-main.2b9fd176.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/covid-19-stats/static/media/image.d7265326.png"
+  }
+]);
