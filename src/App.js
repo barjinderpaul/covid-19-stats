8af10e1e2c76624eba3b9fe3a48a84d7e9aa64b2
barjinderpaul <barjinder.singh@magicpin.in>
2020-04-12 14:06:00 +0530

    Add components to App

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import {Card, Chart, CountryPicker} from './components';
+import styles from './App.module.css';
 
 class App extends React.Component {
     render() {
         return (
-            <div>
-                <h1>Hello stats.</h1>
+            <div className = {styles.container}>
+                <Card />
+                <CountryPicker />
+                <Chart />
             </div>
         )
     }
